e6a143871851d206c5ba2682fdcffbce22776dc6
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2022-09-28 15:11:23 -0300

    DESAFIO 8 - Mostre uma relação dos álbuns produzidos por um artista específico, neste caso 'Elis Regina'

---
+++ desafio8.sql
@@ -0,0 +1,8 @@
+SELECT p.artista AS artista,
+  ms.album AS album
+FROM SpotifyClone.musical_styles AS ms
+INNER JOIN SpotifyClone.performer AS p
+ON p.artista_id = ms.artista_id
+WHERE artista LIKE 'E___%'
+GROUP BY album
+ORDER BY album;
